f74c88e4d8474c0595e317cc37ab4d973b0bff1d
Guillaume-Legault <93203429+Guillaume-Legault@users.noreply.github.com>
2021-11-10 11:02:10 -0500

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,17 @@
-let nombre = 0
+input.onButtonPressed(Button.A, function () {
+    etatactuel = 0
+    basic.clearScreen()
+    basic.showIcon(IconNames.Yes)
+})
 let etatactuel = 0
+etatactuel = 0
 let etatpasse = 0
+basic.showIcon(IconNames.Yes)
 basic.forever(function () {
     etatactuel = pins.digitalReadPin(DigitalPin.P16)
     if (etatactuel != etatpasse) {
         if (etatactuel == 1) {
-            nombre += 1
+            basic.showIcon(IconNames.No)
         }
     }
-    etatpasse = etatactuel
-    basic.showNumber(nombre)
 })
